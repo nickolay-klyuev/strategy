398a9d52127109f3eb4214eee76a7d329a21db91
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-05-27 21:01:38 +0300

    start creating range attack mechanic

---
+++ Assets/Scripts/AttackRangeRadiusController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackRangeRadiusController : MonoBehaviour
+{
+    private UnitProperties unitProperties;
+    private CircleCollider2D circleCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        unitProperties = transform.parent.GetComponent<UnitProperties>();
+        circleCollider = GetComponent<CircleCollider2D>();
+
+        //set attack range for collider
+        circleCollider.radius = unitProperties.attackRange;
+    }
+}
---
+++ Assets/Scripts/UnitProperties.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitProperties : MonoBehaviour
+{
+    public int health = 100;
+    public float attackRange;
+}
