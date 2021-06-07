ad1e428cd10af2b764eaf2281cadb329b710e80b
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-06-07 22:50:31 +0300

    update spawn point

--- Assets/Scripts/SpawnPointController.cs
+++ Assets/Scripts/SpawnPointController.cs
@@ -8,7 +8,7 @@ public class SpawnPointController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(spawnObject);
     }
 
     // Update is called once per frame
