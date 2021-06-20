604871b943798c68d9fd35063e8f0aa750d307f7
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-06-20 13:51:06 +0300

    improve spawn controller

--- Assets/Scripts/SpawnPointController.cs
+++ Assets/Scripts/SpawnPointController.cs
@@ -4,25 +4,12 @@ using UnityEngine;
 
 public class SpawnPointController : MonoBehaviour
 {
-    public GameObject spawnObject;
     public GameObject resourceSystem;
 
-    private int spawnObjectLimit;
-
-    // Start is called before the first frame update
-    void Start()
+    public void SpawnObject(GameObject spawnObject)
     {
-        spawnObjectLimit = spawnObject.GetComponent<UnitProperties>().limit;
-    }
+        int spawnObjectLimit = spawnObject.GetComponent<UnitProperties>().limit;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void SpawnObject()
-    {
         if (GameObject.FindGameObjectsWithTag(spawnObject.name).Length < spawnObjectLimit) // only if unit limit allows
         {
             int cost = spawnObject.GetComponent<UnitProperties>().cost;
