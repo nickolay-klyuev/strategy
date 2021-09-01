2b2c37090697844ee92e74c60da72831f0e53474
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-09-01 23:40:36 +0300

    start making map

--- Assets/ParallaxScript.cs
+++ Assets/ParallaxScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ParallaxScript : MonoBehaviour
 {
+    public string paralaxType = "keyboard"; // mouse, keyboard
     public GameObject cam;
     public float parallaxIndex;
 
@@ -18,11 +19,18 @@ public class ParallaxScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //float distX = cam.transform.position.x * parallaxIndex;
-        //float distY = cam.transform.position.y * parallaxIndex;
+        float distX, distY;
 
-        float distX = (Input.mousePosition.x - Screen.width/2) / 1000 / parallaxIndex;
-        float distY = (Input.mousePosition.y - Screen.height/2) / 1000 / parallaxIndex;
+        if (paralaxType == "keyboard")
+        {
+            distX = cam.transform.position.x * parallaxIndex;
+            distY = cam.transform.position.y * parallaxIndex;
+        }
+        else
+        {
+            distX = (Input.mousePosition.x - Screen.width/2) / 1000 / parallaxIndex;
+            distY = (Input.mousePosition.y - Screen.height/2) / 1000 / parallaxIndex;
+        }
 
         transform.position = new Vector3(startPosition.x + distX, startPosition.y + distY, transform.position.z);
     }
