b7dd8df6fd9b480ba249bc8c49b5e0e17c046721
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-11-04 13:39:03 +0300

    version 0.2.5

--- Assets/Scripts/PostEffectsScript.cs
+++ Assets/Scripts/PostEffectsScript.cs
@@ -11,11 +11,7 @@ public class PostEffectsScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bloom = ScriptableObject.CreateInstance<Bloom>();
-        //bloom.enabled.Override(true);
-        bloom.intensity.Override(5f);
-        bloom.threshold.Override(.6f);
-        volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, bloom);
+        
     }
 
     void OnDestroy()
@@ -30,13 +26,33 @@ public class PostEffectsScript : MonoBehaviour
 
     private IEnumerator GameOverAnimation()
     {
-        while (bloom.threshold.value > 0f || bloom.intensity.value < 100f)
+        bloom = ScriptableObject.CreateInstance<Bloom>();
+        bloom.enabled.Override(true);
+        bloom.intensity.Override(0f);
+        bloom.threshold.Override(0f);
+        
+        ColorParameter bloomColor = new ColorParameter();
+        bloomColor.value = Color.blue;
+        bloom.color.Override(bloomColor);
+
+        volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, bloom);
+
+        while (bloom.intensity.value < 5f)
         {
             yield return new WaitForSeconds(.04f);
-            bloom.threshold.value -= .01f;
-            bloom.intensity.value += 2f;
+            //bloom.threshold.value -= .01f;
+            bloom.intensity.value += 0.1f;
         }
 
         LevelsController.LoadNextScene();
     }
 }
+
+/*
+    Just for example: 
+
+    Bloom bloom = postProcessVolume.profile.GetSetting<UnityEngine.Rendering.PostProcessing.Bloom>();
+    var colorParameter = new UnityEngine.Rendering.PostProcessing.ColorParameter();
+    colorParameter.value = Color.red;
+    bloom.color.Override(colorParameter);
+*/
