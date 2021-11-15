8a0cf3d65b3ab706e54a386675fcd7a8c5dfc145
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-11-15 14:18:06 +0300

    add gather amount text above generators

--- Assets/Buildings/Generator/GeneratorScript.cs
+++ Assets/Buildings/Generator/GeneratorScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GeneratorScript : MonoBehaviour
 {
@@ -8,13 +9,38 @@ public class GeneratorScript : MonoBehaviour
     public float gatherTime = 10f;
 
     private UnitProperties properties;
+    private TextMeshPro gatherAmountText;
+    private Vector3 initTextPos;
+    private bool textAnimation = false;
 
     // Start is called before the first frame update
     void Start()
     {
         properties = GetComponent<UnitProperties>();
 
+        gatherAmountText = GetComponentInChildren<TextMeshPro>();
+        if (gatherAmountText != null) // enemies generators don't have textMshPro TODO: maybe I will remove resources system for enemies
+        {
+            initTextPos = gatherAmountText.transform.position;
+            gatherAmountText.gameObject.SetActive(false);
+        }
+
         InvokeRepeating("GatherResource", gatherTime, gatherTime);
+    }
+
+    void FixedUpdate()
+    {
+        if (textAnimation)
+        {
+            gatherAmountText.transform.Translate(Vector3.up * Time.deltaTime);
+
+            if (gatherAmountText.transform.position.y >= initTextPos.y + 1f)
+            {
+                textAnimation = false;
+                gatherAmountText.gameObject.SetActive(false);
+                gatherAmountText.transform.position = initTextPos;
+            }
+        }
     }
 
     private void GatherResource()
@@ -26,6 +52,10 @@ public class GeneratorScript : MonoBehaviour
         else
         {
             ResourceSystem.GatherResource(resourceAmount);
+
+            gatherAmountText.text = $"+{resourceAmount}";
+            gatherAmountText.gameObject.SetActive(true);
+            textAnimation = true;
         }
     }
 }
